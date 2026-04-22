fde9e73cc7d2c10624a00719d52d760c8899a4ba
jniffene <james.niffenegger@nlr.gov>
2026-04-22 17:39:33 -0600

    TIGER Performance Model V1
    1st attempt at using the H2I format

--- h2integrate/converters/water_power/tiger_tidal.py
+++ h2integrate/converters/water_power/tiger_tidal.py
@@ -7,6 +7,7 @@ from h2integrate.core.model_baseclasses import (
     CostModelBaseConfig,
     PerformanceModelBaseClass,
 )
+import numpy as np
 
 
 @define(kw_only=True)
@@ -32,8 +33,6 @@ class TigerTidalPerformanceConfig(BaseConfig):
     device_rating_kw: float = field(validator=gt_zero)
     num_devices: int = field(validator=gt_zero)
     rotor_radius: float = field(validator=gt_zero)
-    water_ph: float = field(default=8.0, validator=gt_zero)
-
 
 ### Tiger tidal performance model
 class TigerTidalPerformanceModel(PerformanceModelBaseClass):
@@ -85,35 +84,62 @@ class TigerTidalPerformanceModel(PerformanceModelBaseClass):
         )
 
         self.add_input(
-            "device_rating",
+            "device_rating_kw",
             val=self.config.device_rating_kw,
             units="kW",
             desc="Rated power of the tidal energy device",
         )
 
-        #### Can add other outputs if you want them
-        # self.add_output(
-        #     "power_curve",
-        #     units="kW",
-        #     desc="Power curve of the tidal energy device as a function of tidal velocity",
-        # )
+        ### Can add other outputs if you want them
+        self.add_output(
+            "power_curve",
+            units="kW",
+            desc="Power curve of the tidal energy device as a function of tidal velocity",
+        )
+
+        self.add_output(
+            "electricity_out_w",
+            units = "W",
+            desc="Hourly output electricity (W)"
+        )
+
 
     def compute(self, inputs, outputs):
         # assign resource to tidal model
-        inputs["tidal_velocity"]
+        velocity_mPs = inputs["tidal_velocity"]
+
+        # simplify rotor radius and device capacity inputs
+        R_r = inputs["rotor_radius"][0]
+        P_t = inputs["device_rating_kw"][0]
 
         # calculate system capacity
-        system_capacity_kw = inputs["num_devices"][0] * inputs["device_rating"][0]
+        system_capacity_kw = inputs["num_devices"][0] * inputs["device_rating_kw"][0]
 
         # run the necessary calculations
 
         ##### Add tiger performance model calculations here #####
+        
+        # constants 
+        cut_in_velocity = 0.5 # (m/s) cut in velocity
+        cut_out_velocity = 4 # (m/s) cut out velocity
+        C_perf = 0.41 # coefficient of performance
+        density_seawater = 1023 # (kg/m3)
+
+        # calculate power production
+        pwr_w = np.zeros(len(velocity_mPs))
+
+        for i in range(len(velocity_mPs)):
+            if velocity_mPs[i] >= cut_in_velocity and velocity_mPs[i] <= cut_out_velocity:
+                pwr_w[i] = 0.5 * density_seawater * C_perf * (np.pi * R_r**2) * velocity_mPs[i]**3
+                
+                if pwr_w[i] / 1000 > P_t:
+                    pwr_w[i] = P_t * 1000
 
         ### outputs from the model
-        outputs["electricity_out"] = 0  # Add timeseries of the power output here
+        outputs["electricity_out_w"] = pwr_w  # Add timeseries of the power output here
         outputs["rated_electricity_production"] = system_capacity_kw
 
-        outputs["total_electricity_produced"] = outputs["electricity_out"].sum() * (self.dt / 3600)
+        outputs["total_electricity_produced"] = outputs["electricity_out_w"].sum() * (self.dt / 3600)
         outputs["annual_electricity_produced"] = 0
 
         outputs["capacity_factor"] = (
@@ -123,12 +149,28 @@ class TigerTidalPerformanceModel(PerformanceModelBaseClass):
 
 @define(kw_only=True)
 class TigerTidalCostConfig(CostModelBaseConfig):
-    """Add summary of config here."""
+    """ 
+
+    Configuration class for TigerTidalCostModel.
+
+    cost_year (int): Cost year in USD (2025)
+    sub_sea_hubs (bool): If true applies cost reductions from improved designs and supply chains of subsea hubs
+    standard_wet_mates (bool): If true applies cost reductions from standardized designs for wet mate connectors used for tidal turbines
+    piled_foundations (bool): If true applies cost reductions from piled foundations becoming more standard vs monopile moorings
+    advanced_blade_mats (bool): If true applies cost reductions from using advanced blade materials
+    cost_reduction_of_advanced_blade_mats (float): Reduction in blade capital cost if advanced blade materials are considered
+    
+    """
 
     ##### ADD COST MODEL INPUTS HERE
 
     # if the cost year has to be a specific year update it here
-    cost_year: int = field(default=2024, converter=int, validator=must_equal(2024))
+    cost_year: int = field(default=2025, converter=int, validator=must_equal(2025))
+    sub_sea_hubs: bool = field(default=False) 
+    standard_wet_mates: bool = field(default=False) 
+    piled_foundations: bool = field(default=False) 
+    advanced_blade_mats: bool = field(default=False)
+    cost_reduction_of_advanced_blade_mats: float = field(default = 0.5, validator=gt_zero)
 
 
 class TigerTidalCostModel(CostModelBaseClass):
