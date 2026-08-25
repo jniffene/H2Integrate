b8774904dadf1c5e7380bdcff3643cc7fb10fbef
jniffene <james.niffenegger@nlr.gov>
2026-08-25 13:16:33 -0600

    Updated Hybrid Plots

--- examples/tidal_oae_doe/tiger_wind/line_plot_hybrid_1cap_V3.py
+++ examples/tidal_oae_doe/tiger_wind/line_plot_hybrid_1cap_V3.py
@@ -11,7 +11,7 @@ sensCSVfile = "outputs/hybrid_1cap_modRM1_3,4MW_30-50-70_wind_V2.csv"
 turbineName = "50 Unit Hybrid Arrays: 3.4 MW Wind & 3.4 MW RM1"
 
 # Define the values used on the x,y,z axes
-relVals = ['tidal.rotor_radius (m)', 'tidal.device_rating (kW)', 'finance_subgroup_electricity.LCOE (USD/kW/h)', 'oae.carbon_credit_value (USD/t)']
+relVals = ['tidal.rotor_radius (m)', 'tidal.device_rating (kW)', 'finance_subgroup_electricity.LCOE (USD/kW/h)', 'oae.carbon_credit_value (USD/t)', 'oae.profitability_index (unitless)']
 
 # Define the sensitivity variable (just number of tidal devices)
 sens_vars = ["tidal.num_devices"]
@@ -22,15 +22,15 @@ for i in range(len(sens_vars)):
 
 # Obtain wind data for comparison
 windCSVfile_CookInlet = "../wind_only/outputs/100kt_50x_3,4MW_wind_Cook_Inlet_1M_rca.csv"
-windVals = ['finance_subgroup_electricity.LCOE (USD/kW/h)', 'oae.carbon_credit_value (USD/t)']
+windVals = ['finance_subgroup_electricity.LCOE (USD/kW/h)', 'oae.carbon_credit_value (USD/t)', 'oae.profitability_index (unitless)']
 df_wind_CI = pd.read_csv(windCSVfile_CookInlet, usecols=windVals)
 r_start = 10
 r_end = 30
 r_step = 1
 rotor_radii = np.arange(r_start, r_end+r_step, r_step)
-keyVals_wind_CookInlet = df_wind_CI.to_numpy()[0] * np.ones([len(rotor_radii),2])
+keyVals_wind_CookInlet = df_wind_CI.to_numpy()[0] * np.ones([len(rotor_radii),3])
 
-# Create function to rearrange data for radii and capacities values for LCOE and BECC for the line plots
+# Create function to rearrange data for radii and capacities values for LCOE, BECC, and PI for the line plots
 def rad_and_cap_for_lcoe_and_becc(csvFileName, relCols=relVals, nrows=None, skiprows=None, modVar="", modType=""):
     # Create dataframe
     df = pd.read_csv(csvFileName, usecols=relCols, nrows=nrows, skiprows=skiprows)
@@ -47,12 +47,14 @@ def rad_and_cap_for_lcoe_and_becc(csvFileName, relCols=relVals, nrows=None, skip
     # Rearrange target results to be rows as radii and cols as capacities
     lcoe_results = np.zeros((len(radii), len(capacities)))
     becc_results = np.zeros((len(radii), len(capacities)))
+    pi_results = np.zeros((len(radii), len(capacities)))
     for i in range(len(mat[:,0])):
         if mat[i,1] in capacities:
             r = radii.index(mat[i,0])
             c = capacities.index(mat[i,1])
             lcoe_results[r,c] = mat[i,2]
             becc_results[r,c] = mat[i,3]
+            pi_results[r,c] = mat[i,4]
 
     caseDict = {
         "modVar": modVar,
@@ -61,6 +63,7 @@ def rad_and_cap_for_lcoe_and_becc(csvFileName, relCols=relVals, nrows=None, skip
         "capacities": capacities,
         "lcoe": lcoe_results,
         "becc": becc_results,
+        "pi": pi_results,
         
     }
     
@@ -88,9 +91,13 @@ for i in range(len(sens_vars)*3):
     sensDict[i+1] = rad_and_cap_for_lcoe_and_becc(sensCSVfile, nrows=rp, skiprows=rowsSkipped, modVar=sensModVars[i], modType=sensModTypes[i])
 
 # Plot key results for the cases
-keyResults = ["lcoe", "becc"]
-keyFullNames = ["LCOE ($/kWh)", r"Break-Even Carbon Credit Cost (\$/tCO$_2$)"]
-keyNames = ["LCOE", "Break-Even Carbon Credit Cost\n"]
+# keyResults = ["lcoe", "becc", "pi"]
+# keyFullNames = ["LCOE ($/kWh)", r"Break-Even Carbon Credit Cost (\$/tCO$_2$)", "Profitability Index"]
+# keyNames = ["LCOE", "Break-Even Carbon Credit Cost\n", "Profitability Index"]
+
+keyResults = ["pi"]
+keyFullNames = ["Profitability Index"]
+keyNames = ["Profitability Index"]
 
 # Ensure colors are consistent
 cmap = plt.colormaps['tab10']
@@ -134,10 +141,10 @@ for k in range(len(keyResults)):
         plt.xlabel("Tidal Rotor Diameter (m)")
         plt.ylabel(keyFullNames[k])
         plt.title(f"{keyNames[k]} vs Tidal Rotor Diameter & Device Capacity\n({turbineName})")
-        plt.legend(["  0 Wind | 50 Tidal", "15 Wind | 35 Tidal", "25 Wind | 25 Tidal", "35 Wind | 15 Tidal"], loc='upper right')
+        plt.legend(["  0 Wind | 50 Tidal", "15 Wind | 35 Tidal", "25 Wind | 25 Tidal", "35 Wind | 15 Tidal"], loc='center left')
         plt.minorticks_on()
         plt.grid(which='major', linestyle='-', linewidth='0.8', color='darkgray')
-        plt.plot(np.multiply(rotor_radii,2), keyVals_wind_CookInlet[:,k], color='grey', linestyle='-')
+        plt.plot(np.multiply(rotor_radii,2), keyVals_wind_CookInlet[:,k+2], color='grey', linestyle='-')
         # plt.show()
         plt.savefig(f"{turbine}_{keyResults[k]}_hybrid_30-50-70_wind_1cap_3,4MW_V2.png", dpi = 300, bbox_inches='tight')
         plt.close()
